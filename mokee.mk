3f2379b954e1dd7b7e74d80e1ebc90dd3386c3ee
martincz <martincz@mokeedev.com>
2015-03-19 01:47:39 +0800

    titan: mokee bringup

---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/titan/full_titan.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := titan
+PRODUCT_NAME := mk_titan
